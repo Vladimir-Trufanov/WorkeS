f0a944deb6c82966e98b1f054755a01321ea071c
Vladimir-Trufanov <tve@karelia.ru>
2019-11-11 16:47:09 +0300

    Первая проба с аяксом

---
+++ www/_proba/save.php
@@ -0,0 +1,22 @@
+<?php
+   $SiteRoot = $_SERVER['DOCUMENT_ROOT'];       // Корневой каталог сайта
+   //echo "ajaks\r\n";
+   alert('ajaks');
+
+   
+   /*
+   $pathBase='sqlite:'.$SiteRoot.'/sort.db3';                                          
+   $db = new PDO($pathBase);
+   //echo "ajaks\r\n";
+   $pos_new = 100;
+   foreach($_POST['masiv'] as $item)
+   {
+      //$res = mysql_query("UPDATE `sortable` SET `position`='{$pos_new}' WHERE `id`='{$item}'");
+      $sql="UPDATE `sortable` SET `position`='{$pos_new}' WHERE `id`='{$item}'";
+      $st = $db->query($sql);
+      //$results = $st->fetchAll();
+
+      $pos_new++;
+   }
+   */
+?>
