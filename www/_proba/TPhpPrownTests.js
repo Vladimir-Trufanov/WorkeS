fa04f67868f9dbbb099bb8b2ecef78970f954f68
Vladimir-Trufanov <tve@karelia.ru>
2020-02-29 20:46:01 +0300

    Закончен пример простого аякса

---
+++ www/_proba/TPhpPrownTests.js
@@ -0,0 +1,23 @@
+function gali(varName)
+{
+   //alert(varName);
+   var $mew=varName;
+            
+   $.ajax
+   ({
+      url: 'save.php',
+      type: 'POST',
+      data: {mess:$mew},
+      //async: false,
+      error: function()
+      {
+         $('#res').text("Ошибка!");
+      },
+      success: function(message)
+      {
+         //alert(message);
+         console.log(message);
+         $('#res').show().text("Сохранено!").fadeOut(1000);
+      }
+   });
+}
--- www/_proba/save.php
+++ www/_proba/save.php
@@ -1,22 +1,5 @@
 <?php
-   $SiteRoot = $_SERVER['DOCUMENT_ROOT'];       // Корневой каталог сайта
-   //echo "ajaks\r\n";
-   echo 'Получил '.$_POST['masiv'].'! Возвращаю вместе с ';
-   $message='Well!';
+   echo 'Получил '.$_POST['mess'].'! Возвращаю вместе с ';
+   $message='Welli!';
    exit($message);
-   /*
-   $pathBase='sqlite:'.$SiteRoot.'/sort.db3';                                          
-   $db = new PDO($pathBase);
-   //echo "ajaks\r\n";
-   $pos_new = 100;
-   foreach($_POST['masiv'] as $item)
-   {
-      //$res = mysql_query("UPDATE `sortable` SET `position`='{$pos_new}' WHERE `id`='{$item}'");
-      $sql="UPDATE `sortable` SET `position`='{$pos_new}' WHERE `id`='{$item}'";
-      $st = $db->query($sql);
-      //$results = $st->fetchAll();
-
-      $pos_new++;
-   }
-   */
 ?>
