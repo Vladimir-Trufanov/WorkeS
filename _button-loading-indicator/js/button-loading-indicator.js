7084399fc3bb03f426446fd4460b855e22fe34a6
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2021-11-23 15:57:44 +0300

    Добавлены два примера кнопок

---
+++ _button-loading-indicator/js/button-loading-indicator.js
@@ -0,0 +1,30 @@
+'use strict';
+
+$(document).ready(function () {
+   var $button = $('button[type="submit"]');
+   $button.on('click', function () 
+   {
+      var $this = $(this);
+      if ($this.hasClass('active') || $this.hasClass('success')) 
+      {
+	     return false;
+      }
+	  $this.addClass('active');
+	  setTimeout(function () 
+      {
+         $this.addClass('loader');
+      }, 125);
+      setTimeout(function () 
+      {
+         $this.removeClass('loader active');
+		 $this.text('Успешно!');
+		 $this.addClass('success animated pulse');
+      }, 1600);
+      setTimeout(function () 
+      {
+         $this.text('Загрузить ещё');
+         $this.removeClass('success animated pulse');
+         $this.blur();
+      }, 2900);
+  });
+});
